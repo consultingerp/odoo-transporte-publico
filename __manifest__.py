92cdf27d84fcd5046c83f7bc5c5f1628f6d323e0
ltaverao95 <lftavera@hotmail.com>
2019-04-09 15:25:36 -0500

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Registro Transporte Público',
+    'version': '1.0',
+    'author': 'Luis Felipe Tavera',
+    'maintainer': 'Luis Felipe Tavera',
+    'website': 'www.github.com/ltavberao95',
+    'license': 'AGPL-3',
+    'category': 'Extra Tools',
+    'depends': [],
+    'summary': '',
+    'data': [
+        'views/'
+    ],
+    'installable': True,
+    'auto_install': False
+}
