5aea6d331cb7de1daf0580ae650e113c9e852395
ltaverao95 <lftavera@hotmail.com>
2019-04-28 18:47:35 -0500

    changed date to datetime in model

--- models/registro_control_salida.py
+++ models/registro_control_salida.py
@@ -9,4 +9,4 @@ class registro_control_salida(models.Model):
     vehiculo_id = fields.Many2one('registro.vehiculo', 'Vehículo', select=True, track_visibility='onchange')
     empresa_id = fields.Many2one('registro.empresa', 'Empresa', select=True, track_visibility='onchange')
     autorizado = fields.Boolean('¿Salida Autorizada?', default=False, required=True)
-    fecha_salida = fields.Date('Fecha Salida', default=fields.Date.today, required=True)
+    fecha_salida = fields.Datetime('Fecha Salida', default=fields.Date.today, required=True)
