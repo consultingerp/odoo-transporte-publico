cc2e380e2eb777569dfaca8f6ccb43ca6a2b74cb
ltaverao95 <lftavera@hotmail.com>
2019-04-28 18:04:35 -0500

    fixed bug

--- models/registro_vehiculo_conductor.py
+++ models/registro_vehiculo_conductor.py
@@ -6,6 +6,6 @@ class registro_vehiculo_conductor(models.Model):
     _name = 'registro.vehiculo_conductor'
     _rec_name = 'name'
 
-    name = fields.Many2one('registro.empresa', 'Empresa', select=True, track_visibility='onchange)
+    name = fields.Many2one('registro.empresa', 'Empresa', select=True, track_visibility='onchange')
     conductor_id = fields.Many2one('registro.conductor', 'Conductor', select=True, track_visibility='onchange')
     vehiculo_id = fields.Many2one('registro.vehiculo', 'Vehículo', select=True, track_visibility='onchange')
