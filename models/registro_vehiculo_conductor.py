f4d4c788cdea63e9dd178bba6f0b92fb85949477
ltaverao95 <lftavera@hotmail.com>
2019-04-28 16:37:32 -0500

    removed required fields

--- models/registro_vehiculo_conductor.py
+++ models/registro_vehiculo_conductor.py
@@ -6,6 +6,6 @@ class registro_vehiculo_conductor(models.Model):
     _name = 'registro.vehiculo_conductor'
     _rec_name = 'name'
 
-    name = fields.Many2one('registro.empresa', 'Empresa', select=True, track_visibility='onchange', required=True)
-    conductor_id = fields.Many2one('registro.conductor', 'Conductor', select=True, track_visibility='onchange', required=True)
-    vehiculo_id = fields.Many2one('registro.vehiculo', 'Vehículo', select=True, track_visibility='onchange', required=True)
+    name = fields.Many2one('registro.empresa', 'Empresa', select=True, track_visibility='onchange)
+    conductor_id = fields.Many2one('registro.conductor', 'Conductor', select=True, track_visibility='onchange')
+    vehiculo_id = fields.Many2one('registro.vehiculo', 'Vehículo', select=True, track_visibility='onchange')
